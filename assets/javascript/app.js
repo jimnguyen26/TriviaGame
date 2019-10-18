1d9402122f721f6fd7490874947999c64a6f741c
Jim <jimmybnguyen@yahoo.com>
2019-10-18 13:05:41 -0700

    btns change color

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,35 +3,34 @@ $(document).ready(function(){
 });
 
 var timeOut = 0;
-var timeLeft = 30;
+var timeLeft = 10;
 var timerId = setInterval(countdown, 1000);
 var seconds = document.getElementById('timer');
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var timeRemaining = "";
+
 var nflTeams = ["Cowboys", "49ers", "Broncos", "Packers"];
 
 function countdown() {
     if (timeLeft === 0) {
         clearTimeout(timerId);
-        alert('Game over!');
+        alert('Correct Answers: ' + correctAnswer + ' Wrong Answers: ' + wrongAnswer);
     } else {
         seconds.innerHTML = timeLeft + ' seconds';
         timeLeft--;
+        timeLeft === timeOut;
     }
 };
 
 $('#team1').on('click', function() {
-if($(this).text() === correctAnswer) {
-    $(this).addClass('btn-success').removeClass('btn-light');
-}
+    this.style.backgroundColor = "green";
     correctAnswer++;
 console.log(correctAnswer);
-alert('Correct!');
 });
 
 $('#team2, #team3, #team4').on('click', function(){
+    this.style.backgroundColor = "red";
 wrongAnswer++;
 console.log(wrongAnswer);
-alert('Wrong!')
 });
